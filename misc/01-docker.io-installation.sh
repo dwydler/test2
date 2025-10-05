71fbd3d9842210648a0439c2fa1086d4da42747a
548194 <dani.wydler@gmail.com>
2025-10-05 16:11:47 +0200

    Update 01-docker.io-installation.sh

--- misc/01-docker.io-installation.sh
+++ misc/01-docker.io-installation.sh
@@ -11,7 +11,6 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 sudo chmod 644/etc/apt/keyrings/docker.gpg
 
-
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
